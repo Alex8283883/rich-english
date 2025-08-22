52b9ce7cca4064995aa8138bc3e1652e1ba587f7
Alex8283883 <89088281+Alex8283883@users.noreply.github.com>
2025-08-22 15:24:30 +0300

    Update royalify.js

--- api/royalify.js
+++ api/royalify.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        model: "command-r",  // works as per your Colab test
+        model: "command-a-03-2025",  // works as per your Colab test
         message: `Rewrite the following into short, elegant modern English. 
 Only return the rewritten sentence, do not explain or add commentary:\n\n${text}`,
       }),
